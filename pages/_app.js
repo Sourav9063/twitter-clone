f759b242cb083c227551132f3d0f13881a97efca
Sourav9063 <53114581+Sourav9063@users.noreply.github.com>
2023-03-29 22:36:20 +0600

    new modal wrapper for showing tweets

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import '@/styles/globals.css'
 import ModalProvider from '@/providers/ModalProvider'
 import { SessionProvider } from 'next-auth/react'
+import SelectedTweetProvider from '@/providers/SelectedTweet'
+import LikedPostsProvider from '@/providers/LikedPosts'
 
 export default function App({ Component, pageProps }) {
 
   return <SessionProvider session={pageProps.session} >
     {/* <ModalProvider> */}
-    <Component {...pageProps} />
+    <SelectedTweetProvider>
+      <LikedPostsProvider>
+        <Component {...pageProps} />
+      </LikedPostsProvider>
+    </SelectedTweetProvider>
     {/* </ModalProvider> */}
   </SessionProvider>
 }
