31fc6552359fae0af94a1e55bb1274144fed4ada
Sourav9063 <53114581+Sourav9063@users.noreply.github.com>
2023-03-31 20:41:25 +0600

    api of follower

---
+++ db/models/followModel.js
@@ -0,0 +1,30 @@
+import { Schema, model, models } from 'mongoose'
+
+
+export const followSchema = new Schema({
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "UserDB",
+        unique: true
+    },
+    following: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "UserDB",
+            default: []
+        }
+    ],
+    follower: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "UserDB",
+            default: []
+
+        }
+    ]
+
+})
+
+
+const FollowDB = models.FollowDB || model("FollowDB", followSchema);
+export default FollowDB
