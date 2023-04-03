1720c81d7ae7576d3fd0d7424e7bdd27addf4a4b
Sourav9063 <53114581+Sourav9063@users.noreply.github.com>
2023-04-03 22:20:46 +0600

    comment edit delete

--- pages/api/comments/[id]/index.js
+++ pages/api/comments/[id]/index.js
@@ -1,5 +1,6 @@
 import connectMongo from "@/db/dbConnect";
 import CommentDB from "@/db/models/commentModel";
+import { getServerSession } from "next-auth";
 
 export default async function handler(req, res) {
   // res.status(200).json({ name: 'John Doe' })
@@ -14,6 +15,34 @@ export default async function handler(req, res) {
       return res.status(200).json({ msg: "Success", comments });
     } catch (error) {
       return res.status(500).json({ msg: "Server error" });
+    }
+  }
+  if (req.method == "DELETE") {
+    const id = req.query;
+    console.log(id);
+    try {
+      // const session = await getServerSession(req, res, authOptions);
+
+      // const comment = await CommentDB.findById(id);
+
+      const [session, comment] = await Promise.all([
+        getServerSession(req, res, authOptions),
+        CommentDB.findById(id),
+      ]);
+
+      if (session.user.id != comment.owner) {
+        return res.status(401).json({ msg: "Not authorized" });
+      }
+
+      if (!comment) {
+        return res.status(404).json({ msg: "Comment not found" });
+      }
+
+      await CommentDB.findByIdAndDelete(id);
+      return res.status(200).json({ msg: "Comment deleted" });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ msg: "Internal server error", error });
     }
   }
 }
