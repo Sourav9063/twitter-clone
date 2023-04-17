3fd0ffe3c6bc282a1c5fa485efa1858a70a5e294
Farhan Mahtab <farhan.mahi1999@gmail.com>
2023-04-17 13:51:26 +0600

    Get message api created

--- pages/api/v2/messages/index.js
+++ pages/api/v2/messages/index.js
@@ -3,7 +3,20 @@ import UserDBV2 from "@/db/modelsV2/userModelV2";
 import MessageDBV2 from "@/db/modelsV2/messageModelV2";
 
 //Get Messages
-const getAllMessages = async (req, res) => {};
+const getAllMessages = async (req, res) => {
+  try {
+    const { senderId, receiverId } = req.query;
+
+    // Find messages that match the sender ID and receiver ID
+    const messages = await MessageDBV2.find({
+      "sender._id": senderId,
+      "receiver._id": receiverId,
+    });
+    res.status(200).json(messages);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
 //post Message
 const postMessages = async (req, res) => {
@@ -14,32 +27,48 @@ const postMessages = async (req, res) => {
     const receiver = await UserDBV2.findOne({ email: receiverEmail });
     console.log(receiver.username);
 
-    const newMessage = await MessageDBV2({
+    const existingMessage = await MessageDBV2.findOne({
       sender: sender._id,
       receiver: receiver._id,
-      messages: [
-        {
-          sender: sender._id,
-          receiver: receiver._id,
-          senderUsername: sender.username,
-          senderEmail,
-          senderImage: sender.image,
-          receiverUsername: receiver.username,
-          receiverEmail,
-          receiverImage: receiver.image,
-          body: body,
-          //image,
-        },
-      ],
     });
 
-    // Save the message to the database
-    const savedMessage = await newMessage
-      .save()
-      .then(() => console.log("message created"));
+    if (existingMessage) {
+      existingMessage.messages.push({
+        sender: sender._id,
+        receiver: receiver._id,
+        senderUsername: sender.username,
+        senderEmail,
+        senderImage: sender.image,
+        receiverUsername: receiver.username,
+        receiverEmail,
+        receiverImage: receiver.image,
+        body: body,
+        //image,
+      });
+      await existingMessage.save();
+      res.status(200).json(existingMessage);
+    } else {
+      const newMessage = await MessageDBV2.create({
+        sender: sender._id,
+        receiver: receiver._id,
+        messages: [
+          {
+            sender: sender._id,
+            receiver: receiver._id,
+            senderUsername: sender.username,
+            senderEmail,
+            senderImage: sender.image,
+            receiverUsername: receiver.username,
+            receiverEmail,
+            receiverImage: receiver.image,
+            body: body,
+            //image,
+          },
+        ],
+      });
 
-    res.status(200).json({ status: true, data: savedMessage });
-    console.log(savedMessage);
+      res.status(201).json(newMessage);
+    }
   } catch (error) {
     res.status(400).json({ success: false, error: error.message });
   }
