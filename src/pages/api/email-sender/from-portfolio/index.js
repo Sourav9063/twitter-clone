ffedd6e71fa4110b091da147b44049cd4938d23d
Sourav9063 <53114581+Sourav9063@users.noreply.github.com>
2023-07-15 02:22:56 +0600

    form-portfolio api added

---
+++ src/pages/api/email-sender/from-portfolio/index.js
@@ -0,0 +1,36 @@
+import { transporter } from "@/helper/nodemailer/nodemailer";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, username, emailSubject, emailBody } = req.body;
+    if (!email || !username || !emailSubject || !emailBody) {
+      return res.status(400).json({ message: "BAD REQUEST" });
+    }
+    console.log(email);
+    //check email validate
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email" });
+    }
+
+    try {
+      let info = await transporter.sendMail({
+        to: "sourav.ahmed5654@gmail.com",
+        from: email,
+        subject: "EMAIL_FROM_PORTFOLIO: " + emailSubject,
+        text: emailBody + "++" + email,
+        html: `<div><h2>${emailSubject}</h2>
+        <hr />
+        <p>${emailBody}</p>
+        <hr />
+        <p style="color:red;">${email}</p>
+        </div>
+        `,
+      });
+      res.status(200).json({ message: "Email sent successfully" });
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+}
